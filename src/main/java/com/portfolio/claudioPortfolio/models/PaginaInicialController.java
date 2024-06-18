25f8c7c9485bfb4686dd93fa250e1e9755436b05
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2024-06-18 11:24:00 -0300

    Responsividade

--- src/main/java/com/portfolio/claudioPortfolio/models/PaginaInicialController.java
+++ src/main/java/com/portfolio/claudioPortfolio/models/PaginaInicialController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller // Faz que esta classe consiga lidar com requisições Web.
 public class PaginaInicialController {
     @GetMapping("/") // Faz que o método só lide com requisições GET.
-    public String irPaginaInicial(Model model){
+    public String irPaginaInicial(Model model) {
         return "paginaInicial";
     }
 }
