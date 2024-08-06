c6d62a0421a3ce1d9056ddd3ff922e1b0ca51d3c
ClaudioRibeiro2 <Claudio_Ribeiro2@estudante.sc.senai.br>
2024-08-06 16:29:27 -0300

    Changing the design

--- front-end/src/components/header/Header.js
+++ front-end/src/components/header/Header.js
@@ -1,20 +1,51 @@
-import './Header.css';
+import "./Header.css";
 
-function Header(){
-    return (
-    <header className="header">
-        <div className="title">
-            <h1>
-                <a href="/">CLAUDIO CONTI R.</a>
-            </h1>
-        </div>
-        <nav className="nav-container">
-            <a href="/about">Sobre mim</a>
-            <a href="/curriculum">Currículo</a>
-            <a href="/notes">Anotações</a>
-        </nav>
+function Header() {
+  const days = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ];
+
+  return (
+    <header className="site-header">
+      <section className="title">
+        <a href="/">
+          <p className="date">
+            <span className="day">
+              {days[new Date().getDay()]}
+              ,&nbsp;
+              {new Date().toLocaleString("pt-BR", { day: "2-digit" })}
+            </span>
+            &nbsp;de&nbsp;
+            <span className="month">
+              {new Date().toLocaleString("pt-BR", { month: "long" })}
+            </span>
+            &nbsp;
+            <span className="year">{new Date().getFullYear()}</span>
+          </p>
+          <h1 className="site-title">CLAUDIO CONTI R.</h1>
+        </a>
+      </section>
+      <nav className="nav-container">
+        <ul>
+          <li>
+            <a href="/about/">SOBRE MIM</a>
+          </li>
+          <li>
+            <a href="/curriculum/">CURRÍCULO</a>
+          </li>
+          <li>
+            <a href="/notes/">ANOTAÇÕES</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-    );
+  );
 }
 
 export default Header;
